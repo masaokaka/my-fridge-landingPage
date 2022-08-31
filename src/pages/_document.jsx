6f6a1581833306f60610e326134606457ce9fcce
masaokaka <osushiiisensu@gmail.com>
2022-08-31 22:43:56 +0900

    update: 設定ファイルなどの追加

---
+++ src/pages/_document.jsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
