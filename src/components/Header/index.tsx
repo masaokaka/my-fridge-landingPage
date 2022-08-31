883ec4283cdea89117c75d941f1950d80e73a9d4
daikiku10 <soccerdk19.910@gmail.com>
2022-08-31 21:52:21 +0900

    update:ヘッダー内のパーツの配置指定

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+/**
+ * ヘッダー部分
+ * @returns コンポーネント
+ */
 const Header = (): JSX.Element => (
-  <header>
-    <h1>ヘッダー</h1>
+  <header className='py-2 px-20 flex justify-between items-center bg-gray-400'>
     <Link href="/">
-      <Image src="/logo.svg" alt="MyFridge Logo" width={80} height={40} />
+      <Image src="/logo.svg" alt="MyFridge Logo" width={80} height={80} />
     </Link>
+    <div className='flex'>
+      <Link href="/about" passHref>
+        <div className='text-base text-orange-150'>サービスについて</div>
+      </Link>
+      <div className='ml-5 text-orange-150'>ログイン</div>
+      <div className='ml-5 text-orange-150'>新規登録</div>
+    </div>
   </header>
 );
 
