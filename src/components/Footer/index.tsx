13c49627ecbfcf1dc8bd19aaa5593c69c2e5b21f
masaokaka <osushiiisensu@gmail.com>
2022-08-31 20:39:22 +0900

    update:フッターの修正

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { memo } from 'react';
 
-const Footer = (): JSX.Element => (
+const Footer = memo(() => (
   <footer>
-    <a href="https://google.com" target="_blank" rel="noopener noreferrer">
-      Powered by{' '}
-      <span>
+    <div className="flex">
+      <p className="mr-10">©️2022 MyFridge. All Rights Reserved.</p>
+      <Link href="/">
         <Image src="/logo.svg" alt="MyFridge Logo" width={80} height={40} />
-      </span>
-    </a>
+      </Link>
+    </div>
   </footer>
-);
+));
+
+Footer.displayName = 'Footer';
 
 export default Footer;
