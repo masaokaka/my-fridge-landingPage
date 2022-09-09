2b268f79bf3d9e382d632502ef91cb940155b035
daikiku10 <soccerdk19.910@gmail.com>
2022-09-09 22:09:21 +0900

    update:aboutページでは「サービスについて」は非表示にする

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 import Button from '../uiParts/Button';
@@ -6,20 +7,25 @@ import Button from '../uiParts/Button';
  * ヘッダー部分
  * @returns コンポーネント
  */
-const Header = (): JSX.Element => (
-  <header className='py-2 px-20 flex justify-between items-center bg-gray-400'>
-    <Link href="/">
-      <Image src="/logo.svg" alt="MyFridge Logo" width={80} height={80} />
-    </Link>
-    <div className='flex items-center'>
-      {/* TODO: aboutページの時は非表示にする */}
-      <Link href="/about" passHref>
-        <div className='text-base text-orange-150'>サービスについて</div>
+const Header = (): JSX.Element => {
+  const router = useRouter();
+  const isAboutPage = router.pathname === '/about';
+  return (
+    <header className='py-2 px-20 flex justify-between items-center bg-gray-400'>
+      <Link href="/">
+        <Image src="/logo.svg" alt="MyFridge Logo" width={80} height={80} />
       </Link>
-      <Button className='ml-5' icon='/login.svg'>ログイン</Button>
-      <Button className='ml-5 bg-orange-sub' icon='/create.svg'>新規登録</Button>
-    </div>
-  </header>
-);
+      <div className='flex items-center'>
+        {!isAboutPage && (
+          <Link href="/about" passHref>
+            <div className='text-base text-orange-150 cursor-pointer'>サービスについて</div>
+          </Link>
+        )}
+        <Button className='ml-5' icon='/login.svg'>ログイン</Button>
+        <Button className='ml-5 bg-orange-sub' icon='/create.svg'>新規登録</Button>
+      </div>
+    </header>
+  )
+};
 
 export default Header;
